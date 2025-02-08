051c71c0ff0d1740e5dea9b48d607f78ec2bf35c
Stella <sarahprog667@gmail.com>
2025-02-08 10:06:40 +0800

    fix debounced mess

--- front-gfl/src/components/Graph.jsx
+++ front-gfl/src/components/Graph.jsx
@@ -13,7 +13,7 @@ import {
 import 'chartjs-adapter-dayjs-4/dist/chartjs-adapter-dayjs-4.esm';
 import zoomPlugin from 'chartjs-plugin-zoom';
 import dayjs from 'dayjs';
-import { useCallback, useEffect, useRef, useState } from 'react';
+import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import { Line } from 'react-chartjs-2';
 import { fetchUrl, SERVER_WATCH } from '../config'
 import annotationPlugin from 'chartjs-plugin-annotation';
@@ -29,21 +29,29 @@ ChartJS.register(
     zoomPlugin,
     annotationPlugin
   );
-function debounce(func, wait, immediate) {
-  let timeout;
-  return function() {
+  function debounce(func, wait, immediate) {
+    let timeout;
+    const debounced = function() {
       const context = this;
       const args = arguments;
-      const later = function() {
-          timeout = null;
-          if (!immediate) func.apply(context, args);
+      const later = () => {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
       };
       const callNow = immediate && !timeout;
       clearTimeout(timeout);
       timeout = setTimeout(later, wait);
       if (callNow) func.apply(context, args);
-  };
-}
+    };
+  
+    debounced.cancel = () => {
+      clearTimeout(timeout);
+      timeout = null;
+    };
+  
+    return debounced;
+  }
+  
 export default function Graph({ onDateChange }){
     const now = dayjs()
     const [ startDate, setStartDate ] = useState(now.subtract(1, 'day'))
@@ -56,26 +64,36 @@ export default function Graph({ onDateChange }){
       onDateChange(startDate, endDate)
     }, [startDate, endDate])
 
-    const setDateCallback = useCallback(debounce((xScale) => {
-      setStartDate(dayjs(xScale.min))
-      setEndDate(dayjs(xScale.max))
-      console.log('X Min:', dayjs(xScale.min), 'X Max:', dayjs(xScale.max));
-    }, 1000, false), [])
+    const debouncedSetDateRef = useRef();
+
+    useEffect(() => {
+      debouncedSetDateRef.current = debounce((xScale) => {
+        setStartDate(dayjs(xScale.min))
+        setEndDate(dayjs(xScale.max))
+      }, 1000, false)
+    
+      return () => {
+        debouncedSetDateRef.current.cancel()
+      }
+    }, []);
 
     const zoomComplete = useCallback(({ chart }) => {
-      setDateCallback(chart.scales.x)
+      debouncedSetDateRef.current(chart.scales.x)
     }, [])
-    const options = {
+    const options = useMemo(() => ({
+      animation: false,
       responsive: true,
       scales: {
         x: {
           type: 'time',
           time: {
-              displayFormats: {
-                  minute: 'MMM d, h:mm a',
-                  hour: 'MMM d, h:mm a',
-                  day: 'MMM d'
-              }
+            displayFormats: {
+              minute: 'MMM DD, h:mm a', 
+              hour: 'MMM DD, ha',       
+              day: 'MMM DD',            
+              week: 'MMM DD',           
+              month: 'MMM YYYY',        
+          }
           },
           ticks: {
               autoSkip: true,
@@ -111,7 +129,7 @@ export default function Graph({ onDateChange }){
             }
           }
       },
-    }
+    }), [annotations])
   
     useEffect(() => {
       if (!startDate.isBefore(endDate)) return
@@ -126,9 +144,11 @@ export default function Graph({ onDateChange }){
       }
       fetchUrl(`/graph/${SERVER_WATCH}/maps`, { params })
       .then(data => data.map(e => ({
-          type: 'line', 
+          type: 'box', 
           xMin: e.started_at,
-          xMax: e.started_at,
+          xMax: e.ended_at,
+          yMin: -1,
+          yMax: 100,
           borderColor: 'rgb(255, 99, 132)',
           borderWidth: 2,
           label: {
@@ -136,7 +156,7 @@ export default function Graph({ onDateChange }){
             content: e.map,
             display: true,
             rotation: 270,
-            position: 'start'
+            position: 'end'
           }
         })))
       .then(anno => setAnnotations({...anno}))
