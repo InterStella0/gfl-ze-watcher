ea0cca84e417ba428df514fdb6fdd01e8f1b5022
Stella <sarahprog667@gmail.com>
2025-02-23 22:18:15 +0800

    added loading state player card

--- front-gfl/src/components/SearchPlayers.jsx
+++ front-gfl/src/components/SearchPlayers.jsx
@@ -5,7 +5,7 @@ import { fetchUrl, secondsToHours } from '../utils';
 import {
     Badge,
     LinearProgress,
-    Pagination,
+    Pagination, Skeleton,
     Typography
 } from '@mui/material';
 import { PlayerAvatar } from './PlayerAvatar';
@@ -77,6 +77,41 @@ function PlayerCard({ player }){
     </ErrorCatch>
 }
 
+
+function PlayerCardLoading(){
+    const randomNameWidth = Math.random() * (250 - 30) + 30;
+    return <Grid size={{xl: 3, lg: 4, md: 6, s: 6, xs: 12}}>
+        <Paper>
+            <Paper
+                elevation={1}
+                sx={{
+                    cursor: 'pointer',
+                    transition: '0.3s',
+                    p: 2,
+                    '&:hover': {
+                        backgroundColor: 'rgba(0, 0, 0, 0.05)',
+                    },
+                    '&:active': {
+                        transform: 'scale(0.98)',
+                    },
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    display: 'flex',
+                    flexDirection: 'column'
+                }}>
+
+                <div style={{display: 'flex', alignContent: 'center', justifyContent: 'center'}}>
+                    <Skeleton variant="circular" width={120} height={120} />
+
+                </div>
+                <Skeleton variant="text" width={randomNameWidth} />
+                <Skeleton variant="text" width={190} />
+            </Paper>
+        </Paper>
+    </Grid>
+}
+
+
 function SearchPlayersDisplay(){
     const [search, setSearch ] = useState(null)
     const [ result, setResult ] = useState([])
@@ -109,20 +144,21 @@ function SearchPlayersDisplay(){
             />
         </div>
         <div style={{minHeight: 'calc(32px)', margin: '1rem'}}>
-        {!loading && search && (search?.trim() === "" || <div style={
-                {display: 'flex', justifyContent: 'space-between',
-                    flexDirection: 'row'}
-        }>
-            <p>Matched {matching} player(s) with the search &#34;{search}&#34;.</p>
-            <Box>
-                <Pagination count={Math.ceil(matching / 40)} variant="outlined" color="primary" page={page} onChange={(_, e) => setPage(e)} />
-            </Box>
-        </div>
-        )}
+            {!loading && search && (search?.trim() === "" || <div style={
+                    {display: 'flex', justifyContent: 'space-between',
+                        flexDirection: 'row'}
+            }>
+                <p>Matched {matching} player(s) with the search &#34;{search}&#34;.</p>
+                <Box>
+                    <Pagination count={Math.ceil(matching / 40)} variant="outlined" color="primary" page={page} onChange={(_, e) => setPage(e)} />
+                </Box>
+            </div>
+            )}
         </div>
         <div style={{padding: '1rem'}}>
             <Grid container spacing={2} sx={{ flexGrow: 1, minHeight: '60vh', margin: '1rem'}}>
-                {result.map(e =>
+                {loading && Array.from({length: 6}).map((_, index) => <PlayerCardLoading key={index} />)}
+                {!loading && result.map(e =>
                         <Grid size={{xl: 3, lg: 4, md: 6, s: 6, xs: 12}} key={e.id}>
                             <Paper><PlayerCard player={e} /></Paper>
                         </Grid>
