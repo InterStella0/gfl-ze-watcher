14273d6901b44860fb984e7ddd772120500c4d68
Stella <sarahprog667@gmail.com>
2025-03-29 17:28:35 +0800

    map size tweaks

--- front-gfl/src/components/maps/MapSessionList.jsx
+++ front-gfl/src/components/maps/MapSessionList.jsx
@@ -46,11 +46,15 @@ function AllSessions(){
     return <>
         <Paper sx={{p: '1rem'}}>
             <Grid container>
-                <Grid size={5}>
+                <Grid size={{lg: 4, md: 5, sm: 4, xs: 12}}>
                     <Typography variant="h6" component="h2" color="primary" fontWeight={700} textAlign="start">Sessions</Typography>
                 </Grid>
-                <Grid size={7}>
-                    <Box display="flex" alignItems="right" justifyContent="right">
+                <Grid size={{lg: 8, md: 7, sm: 8, xs: 12}}>
+                    <Box display="flex" alignItems={{md: "right", xs: "center"}} justifyContent={{
+                        md: "right",
+                        sm: 'right',
+                        xs: "center"
+                    }} width="100%">
                         <PaginationPage page={page} totalItems={totalSessions} perPage={5} setPage={setPage} />
                     </Box>
                 </Grid>
@@ -99,17 +103,17 @@ function SessionGraph({ session }){
     const { setShowPlayer } = useContext(MapSessionContext)
     const startedAt = dayjs(session.started_at)
     const endedAt = session.ended_at? dayjs(session.ended_at): dayjs()
-
+    const textSizes = {lg: '1rem', md: '.9rem', sm: '.8rem', xs: '.6rem'}
     return <Paper sx={{m: '.5rem' }} elevation={0}>
         <Grid container>
-            <Grid size={6}>
-                <Typography sx={{m: '.5rem', textAlign: 'start'}}>Session #{session.time_id}</Typography>
+            <Grid size={5}>
+                <Typography sx={{m: '.5rem', textAlign: 'start'}} fontSize={{md: '1rem', sm: '.7rem', xs: '.7rem'}}>Session #{session.time_id}</Typography>
             </Grid>
-            <Grid size={6}>
+            <Grid size={7}>
                 <Box alignItems="right" display="flex" flexDirection="row" justifyContent="right" gap=".5rem" m=".5rem">
-                    <Typography>{dayjs().diff(startedAt, 'd') < 1? startedAt.fromNow(): startedAt.format('lll')}</Typography>
-                    <Typography>•</Typography>
-                    <Typography>{endedAt.diff(startedAt, "m")}mins</Typography>
+                    <Typography fontSize={textSizes}>{dayjs().diff(startedAt, 'd') < 1? startedAt.fromNow(): startedAt.format('lll')}</Typography>
+                    <Typography fontSize={textSizes}>•</Typography>
+                    <Typography fontSize={textSizes}>{endedAt.diff(startedAt, "m")}mins</Typography>
                 </Box>
             </Grid>
             <Grid size={12}>
@@ -117,7 +121,7 @@ function SessionGraph({ session }){
                     <SessionPlayedGraph start={startedAt} end={endedAt} />
                 </Paper>
             </Grid>
-            <Grid size={4}>
+            <Grid size={6}>
                 <Box  alignItems="start" display="flex" sx={{m: '.5rem', mt: '0'}}>
                     <Button variant="outlined" size="small" startIcon={<GroupIcon />} onClick={() => {
                         setShowPlayer(session)
