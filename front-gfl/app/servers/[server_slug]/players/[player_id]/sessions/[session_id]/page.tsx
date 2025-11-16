4dbe11097e32296b08f4a5eb737788d2b30c7db5
InterStella0 <70153286+InterStella0@users.noreply.github.com>
2025-11-16 16:19:51 +0800

    add metadata of player sessions

--- front-gfl/app/servers/[server_slug]/players/[player_id]/sessions/[session_id]/page.tsx
+++ front-gfl/app/servers/[server_slug]/players/[player_id]/sessions/[session_id]/page.tsx
@@ -9,12 +9,18 @@ import {
     getMutualSessions,
     getServerGraph,
     getServerSlug, getSessionInfo,
-    PlayerSessionMapPlayed
+    PlayerSessionMapPlayed, SessionInfo
 } from "../../../../util";
-import {fetchServerUrl, getMapImage} from "utils/generalUtils";
+import { fetchServerUrl, formatHours, formatTitle, getMapImage} from "utils/generalUtils";
 import {getPlayerDetailed, PlayerInfo} from "../../util";
 import MutualSessionsDisplay from "components/sessions/MutualSessionsDisplay";
 import {notFound} from "next/navigation";
+import {Metadata} from "next";
+import { PlayerProfilePicture} from "types/players.ts";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+
+dayjs.extend(relativeTime)
 
 async function getMapImages(server_id: string, player_id: string, session_id: string): Promise<Record<string, string>> {
     const mapsData: PlayerSessionMapPlayed[] = await fetchServerUrl(server_id, `/players/${player_id}/sessions/${session_id}/maps`);
@@ -31,7 +37,93 @@ async function getMapImages(server_id: string, player_id: string, session_id: st
     const imageMap = imageResults.reduce((acc, curr) => ({ ...acc, ...curr }), {});
     return imageMap as Record<string, string>;
 }
+export async function generateMetadata({ params}: {
+    params: { server_slug: string, player_id: string, session_id: string }
+}): Promise<Metadata> {
+    const { server_slug, player_id, session_id } = await params;
+    const server = await getServerSlug(server_slug);
+    let player: PlayerInfo | null = null
+    let info: SessionInfo<"player"> | null = null;
+    try{
+        player = await getPlayerDetailed(server.id, player_id)
+        info = await getSessionInfo(server.id, session_id, "player", player_id)
+    }catch(error){
+        if (error.code === 202){
+            return {
+                title: formatTitle(player_id),
+                description: "The player is still being calculated. Please come back later~",
+            }
+        }else if (error.code === 404){
+            return {
+                title: "ZE Graph",
+                description: `View player activities on ${server.community.name}`
+            };
+        }else{
+            return {}
+        }
+    }
+    let description = ``
+    if (info.ended_at){
+        description += `They were playing for ${formatHours(dayjs(info.ended_at).diff(dayjs(info.started_at), 'seconds', true))}.`
+    }else{
+        description += `They are playing for ${formatHours(dayjs().diff(dayjs(info.started_at), 'seconds', true))}.`
+    }
+    try{
+        const serverCounts = await getServerGraph(server.id, session_id, player.id, "player")
+        const playerCounts = serverCounts.map(e => e.player_count)
+        const [min, max] = [Math.min(...playerCounts), Math.max(...playerCounts)]
+        description += ` Player count ranges from ${min} to ${max}.`
+    }catch (error){}
 
+    try{
+        const maps = await getMapsDataSession(server.id, player.id, session_id)
+        if (maps && maps.length > 0) {
+            const mapNames = maps.map(e => e.map)
+            const firstTwo = mapNames.slice(0, 2).join(", ")
+
+            const remaining = mapNames.length - 2
+            const moreText = remaining > 0 ? ` and ${remaining} more` : ""
+
+            description += ` Played through ${firstTwo}${moreText}.`
+        }
+    }catch(error){}
+
+    if (player.online_since){
+        description += ` Currently online in the server since ${dayjs(player.online_since).fromNow()}.`;
+    }else{
+        description += ` Last online in the server since ${dayjs(player.last_played).fromNow()}.`;
+    }
+    try{
+        const players = await getMutualSessions(server.id, session_id, "player", player_id)
+
+        description += ` They have seen ${players.length} players in this session.`
+    }catch(error){}
+    let image = ""
+    try{
+        const pfp: PlayerProfilePicture | null = await fetchServerUrl(server.id, `/players/${player.id}/pfp`)
+        image = pfp.full
+    }catch(error){
+
+    }
+
+    const title = formatTitle(`${player.name}'s Session on ${server.community.name}`)
+    return {
+        title: title,
+        description: description,
+        openGraph: {
+            type: "website",
+            title,
+            description,
+            images: [image],
+        },
+        twitter: {
+            card: "summary_large_image",
+            title,
+            description,
+            images: [image],
+        },
+    }
+}
 export default async function Page({ params }){
     const { player_id, server_slug, session_id } = await params
     try{
