ade5dcc01fbfc0aa8862a027ce0ddb4f90253f3e
InterStella0 <70153286+InterStella0@users.noreply.github.com>
2025-11-16 19:27:14 +0800

    added basic metadata on all pages

--- front-gfl/app/live/page.tsx
+++ front-gfl/app/live/page.tsx
@@ -1,6 +1,18 @@
 import LiveServerTrackerPage from "./TrackerPage";
 import getServerUser from "../getServerUser.ts";
 import {cookies} from "next/headers";
+import {Metadata} from "next";
+import {getServerSlug} from "../servers/[server_slug]/util.ts";
+import {ServerPlayersStatistic} from "types/players.ts";
+import {fetchServerUrl, formatTitle} from "utils/generalUtils.ts";
+
+
+export async function generateMetadata(): Promise<Metadata> {
+    return {
+        title: formatTitle(`Live Tracker`),
+        description: "All player data updates are streamed through here."
+    }
+}
 
 export default async function Page(){
     const user = await getServerUser(cookies());
--- front-gfl/app/servers/[server_slug]/maps/page.tsx
+++ front-gfl/app/servers/[server_slug]/maps/page.tsx
@@ -1,11 +1,39 @@
 import {Container} from "@mui/material";
 import CurrentMatch from "components/maps/CurrentMatch";
-import {getMapImage} from "utils/generalUtils";
+import {fetchServerUrl, formatTitle, getMapImage} from "utils/generalUtils";
 import {getServerSlug} from "../util";
 import MapsSearchIndex from "./MapsSearchIndex";
 import getServerUser from "../../../getServerUser";
 import {cookies} from "next/headers";
 import {getMatchNow} from "./util";
+import {Metadata} from "next";
+import {MapPlayedPaginated} from "types/maps.ts";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+dayjs.extend(relativeTime)
+
+export async function generateMetadata({ params}: {
+    params: Promise<{ server_slug: string }>
+}): Promise<Metadata> {
+    const { server_slug } = await params
+
+    const server = await getServerSlug(server_slug)
+    if (!server)
+        return {}
+
+    let description = `Play zombie escape on ${server.community.name} at ${server.fullIp}.`
+    try{
+        const parameters = { page: 1, sorted_by: 'last_played' }
+        const data: MapPlayedPaginated = await fetchServerUrl(server.id, '/maps/last/sessions', { params: parameters });
+        const latestMap = data.maps[0]
+        description += ` There are over ${data.total_maps} maps that has been played! Last played ${latestMap.map} at ${dayjs(latestMap.last_played).fromNow()}.`
+    }catch(e){}
+
+    return {
+        title: formatTitle(`${server.community.name} Maps`),
+        description: description
+    }
+}
 
 export default async function Page({ params }){
     const { server_slug } = await params;
